41a4dd0d9b0c4b62381d8ff3ae85b922efdf85fc
a_teammate <madoe3@web.de>
2018-11-15 20:42:29 +0100

    fix conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -10,11 +10,3 @@ class Pkg(ConanFile):
         cmake.configure(source_folder=".")
         cmake.build()
 
-    def package(self):
-        self.copy("*.h", src="src", dst="include")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
-
-    def package_info(self):
-        self.cpp_info.libs = ["databaseio"]
-
