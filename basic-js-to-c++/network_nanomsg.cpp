8b0b956c5f87c2abeef71cae445b3f65e408d195
a_teammate <madoe3@web.de>
2018-11-15 20:50:06 +0100

    add network_nanomsg as skeleton for the nanomsg code

---
+++ basic-js-to-c++/network_nanomsg.cpp
@@ -0,0 +1,10 @@
+#include "network_nanomsg.h"
+
+
+void network_nanomsg::pull_blocking(const uint8_t *data, size_t &size) {
+
+}
+
+void network_nanomsg::push_blocking(const uint8_t *data, size_t size) {
+
+}
